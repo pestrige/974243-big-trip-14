9291f9f79d1fd66af12bf17de8975f04a45563a6
pestrige <pestrige@gmail.com>
2021-05-22 11:08:57 +0300

    Добавил сборку webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/js'),
+  },
+  devtool: 'sourcemap',
+  devServer: {
+    publicPath: '/js/',
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+  }
+};
